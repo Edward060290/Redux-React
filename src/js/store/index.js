091636382d5ca2b5cab57a9f0b6fcb6c56ae0d7d
Edward060290 <barranoneduardo@gmail.com>
2018-10-10 01:15:33 -0500

    create a reducer folder

---
+++ src/js/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from "redux";
+import rootReducer from "../reducers/indesx";
+
+const store = createStore(rootReducer);
+
+export default store;
+
+import { createStore } from "redux";
+import rootReducer from "../reudcers/index";
+
+const store = createStore(rootReducer);
+
+export default store;
